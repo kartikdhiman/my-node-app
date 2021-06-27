f03539625ac9c9358744c23e4a14b109a6541cd8
Kartik Dhiman <kartikjuly@gmail.com>
2021-06-27 16:05:38 +0530

    feat: logout user

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -5,16 +5,20 @@ const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
     const payload = jwt.verify(token, 'thisistaskmanagerapi');
-    const user = await User.findOne({ _id: payload._id, 'tokens.token': token });
+    const user = await User.findOne({
+      _id: payload._id,
+      'tokens.token': token,
+    });
 
     if (!user) {
       throw new Error();
     }
+    req.token = token;
     req.user = user;
     next();
   } catch (error) {
     res.status(401).send({ error: 'Please authenticate' });
   }
-}
+};
 
 module.exports = auth;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -23,7 +23,27 @@ router.post('/users/login', async (req, res) => {
   } catch (error) {
     res.status(400).send();
   }
-})
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
 
 router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
